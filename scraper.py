f7ae719b49fc5d5953f8221bea2308fd14f8c1ea
Aditya Singh <48862454+aditya29may@users.noreply.github.com>
2021-12-12 16:11:14 +0000

    used selenium

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 YOUTUBE_TRENDING_URL = 'https://www.youtube.com/feed/trending'                     
 
@@ -14,17 +15,22 @@ def get_driver():
   driver = webdriver.Chrome(options=chrome_options)
   return driver
 
+def get_videos(driver):
+  VIDEO_DIV_TAG = 'ytd-video-renderer'
+  driver.get(YOUTUBE_TRENDING_URL)
+  videos = driver.find_elements(By.TAG_NAME, VIDEO_DIV_TAG)
+  return videos
 if __name__=='__main__':
   print('Creating driver')
   driver = get_driver()
 
-  print('Fetching the page')
-  driver.get(YOUTUBE_TRENDING_URL)
-  print('Page title:', driver.title)
+  print('Fetching trending videos')
+  videos = get_videos(driver)
+  #driver.get(YOUTUBE_TRENDING_URL)
+  #print('Page title:', driver.title)
 
-  print('Get the video divs')
-  VIDEO_DIV_CLASS = 'ytd-video-renderer'
-  video_divs = driver.find_elements_by_class_name
-  (VIDEO_DIV_CLASS)
-
-  print(f'Found {len(video_divs)} videos')
+  #print('Get the video divs')
+  #VIDEO_DIV_TAG = 'ytd-video-renderer'
+  # video_divs = driver.find_elements_by_tag_name(VIDEO_DIV_TAG)
+  #video_divs = driver.find_elements(By.TAG_NAME, VIDEO_DIV_TAG)
+  print(f'Found {len(videos)} videos')
